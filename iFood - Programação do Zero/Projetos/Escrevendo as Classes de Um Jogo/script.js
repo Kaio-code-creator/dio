02e9a282e8769f1d59c18bdda64df936f307731a
rodrigo30041983 <kaioasousa12@gmail.com>
2023-12-04 14:56:50 -0300

    .

---
+++ iFood - Programação do Zero/Projetos/Escrevendo as Classes de Um Jogo/script.js
@@ -0,0 +1,34 @@
+// Definição da classe Heroi
+class Heroi {
+    constructor(nome, idade, tipo) {
+        this.nome = nome;
+        this.idade = idade;
+        this.tipo = tipo;
+    }
+
+    atacar() {
+        let ataque;
+        switch (this.tipo) {
+            case 'mago':
+                ataque = 'usou magia';
+                break;
+            case 'guerreiro':
+                ataque = 'usou espada';
+                break;
+            case 'monge':
+                ataque = 'usou artes marciais';
+                break;
+            case 'ninja':
+                ataque = 'usou shuriken';
+                break;
+            default:
+                ataque = 'usou um ataque indefinido';
+        }
+
+        console.log(`O ${this.tipo} atacou usando ${ataque}`);
+    }
+}
+
+// Exemplo de uso da classe Heroi
+const heroiExemplo = new Heroi('Gandalf', 1500, 'mago');
+heroiExemplo.atacar();  // Saída esperada: "O mago atacou usando magia"
