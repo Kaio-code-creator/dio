a8ba50d131fe022148fa8747b6b26d97450e8f24
Kaio-code-creator <kaioasousa12@gmail.com>
2023-12-19 16:14:59 -0300

    .

--- iFood - Desenvol de Jogos/Desafios de codigo/Tomadas de Decisão/script.js
+++ iFood - Desenvol de Jogos/Desafios de codigo/Tomadas de Decisão/script.js
@@ -4,11 +4,11 @@ const nomePersonagem = gets();
 // Solicita ao usuário que escolha entre "Atacar" ou "Fugir":
 const acaoEscolhida = gets();
 
-// Verifica a ação escolhida e exibir a mensagem correspondente:
-while (acaoEscolhida !== "Atacar" && acaoEscolhida !== "Fugir") {
+// Verifica a ação escolhida e exibe a mensagem correspondente:
+if (acaoEscolhida === "Atacar") {
+  console.log(`${nomePersonagem} escolheu Atacar!`);
+} else if (acaoEscolhida === "Fugir") {
+  console.log(`${nomePersonagem} escolheu Fugir!`);
+} else {
   console.log("Tente novamente");
-  acaoEscolhida = gets();
 }
-
-// Exibe a mensagem correspondente:
-console.log(`${nomePersonagem} escolheu ${acaoEscolhida}!`);
